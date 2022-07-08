3751e29f5d4c934882b723e694de27e56d6380ef
ethanchung936 <ethanchung936@gmail.com>
2022-07-08 16:39:32 -0600

    renamed project and app to be more applicable

--- TwitWord/TwitWord/asgi.py
+++ TwitWord/TwitWord/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tweet_counter project.
+ASGI config for TwitWord project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tweet_counter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwitWord.settings')
 
 application = get_asgi_application()
--- TwitWord/word_lookup/apps.py
+++ TwitWord/word_lookup/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WordCountConfig(AppConfig):
+class WordLookupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'word_count'
+    name = 'word_lookup'
