fc58f256787349a9ad3e28ef2c5b91c1cab8b09f
ethanchung936 <ethanchung936@gmail.com>
2022-07-14 23:46:41 -0600

    Added comments and created home page code test

--- TwitWord/word_lookup/tests.py
+++ TwitWord/word_lookup/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Test to see if the home page is working
+class HomePageTest(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/home/')
+        self.assertEquals(response.status_code, 200)
--- TwitWord/word_lookup/urls.py
+++ TwitWord/word_lookup/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+# The home view
 urlpatterns = [
     path('', views.home, name='home'),
 ]
