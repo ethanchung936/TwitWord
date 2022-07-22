0891e6e08eaf1d6c269f84ef527338fbaea27406
ethanchung936 <ethanchung936@gmail.com>
2022-07-21 20:27:50 -0600

    Adjusted redirect links

--- TwitWord/word_lookup/views.py
+++ TwitWord/word_lookup/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .tweet_fetch import get_tweets
-# Create your views here.
-
-# display the home webpage
 
 def home(request):
+    # display the homepage and redirect to the word input page when sent a post request
     if request.method == 'POST':
+        url = reverse('home')
         word = request.POST['word']
-        return HttpResponseRedirect(word)
+        # TODO add word validation
+        return HttpResponseRedirect(url + word)
     return render(request, 'word_lookup/home.html')
 
-# Retrive the input word and process it
 def word_input(request, word):
+    if request.method == 'POST':
+        url = reverse('home')
+        word = request.POST['word']
+        # TODO add word validation
+        return HttpResponseRedirect(url + word)
+    # Retrive the input word and process it
     tweets = get_tweets(word)
     return render(request, 'word_lookup/tweets.html', {'word': word})
