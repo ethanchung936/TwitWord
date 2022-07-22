521fbd2f627f4886bf6ac9d17ea1cd13bfc2ca30
ethanchung936 <ethanchung936@gmail.com>
2022-07-21 19:15:05 -0600

    Made home redirect to a new web page when a POST request is enetered

--- TwitWord/word_lookup/urls.py
+++ TwitWord/word_lookup/urls.py
@@ -5,4 +5,5 @@ from . import views
 # The home view
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<word>/', views.word_input, name='word_input'),
 ]
--- TwitWord/word_lookup/views.py
+++ TwitWord/word_lookup/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from .tweet_fetch import get_tweets
 # Create your views here.
+
+# display the home webpage
+
 def home(request):
-    return render(request , 'word_lookup/home.html')
+    if request.method == 'POST':
+        word = request.POST['word']
+        return HttpResponseRedirect(word)
+    return render(request, 'word_lookup/home.html')
+
+# Retrive the input word and process it
+def word_input(request, word):
+    tweets = get_tweets(word)
+    return render(request, 'word_lookup/tweets.html', {'word': word})
