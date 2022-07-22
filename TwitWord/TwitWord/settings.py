cd5de9e2fc8b74ac426f017e498a72cbfff440db
ethanchung936 <ethanchung936@gmail.com>
2022-07-21 19:13:55 -0600

    shortened the word_lookup address in INSTALLED_APPS

--- TwitWord/TwitWord/settings.py
+++ TwitWord/TwitWord/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my applications
-    'word_lookup.apps.WordLookupConfig',
+    'word_lookup',
 ]
 
 MIDDLEWARE = [
