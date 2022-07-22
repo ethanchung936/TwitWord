8bdc7919b37d82b979d79b1e188b24803f04c08d
ethanchung936 <ethanchung936@gmail.com>
2022-07-21 20:26:25 -0600

    added redirect and get_tweets() tests

--- TwitWord/word_lookup/tests.py
+++ TwitWord/word_lookup/tests.py
@@ -1,9 +1,18 @@
 from django.test import TestCase
-
-# Create your tests here.
+from .tweet_fetch import get_tweets
 
 # Test to see if the home page is working
 class HomePageTest(TestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/home/')
         self.assertEquals(response.status_code, 200)
+        
+    def test_home_page_redirect(self):
+        response = self.client.get('/home/test')
+        self.assertEquals(response.status_code, 301)
+        
+# Test get_tweets function
+class GetTweetsTest(TestCase):
+    def test_get_tweets(self):
+        tweets = get_tweets('test')
+        self.assertEquals(len(tweets), 5)
