4ea0243f9d8bdf52ffcebaee0ffb5c7def7a61ca
ethanchung936 <ethanchung936@gmail.com>
2022-08-06 14:55:05 -0600

    Added filters to search and implemented get_ids()

--- TwitWord/word_lookup/tweet_fetch.py
+++ TwitWord/word_lookup/tweet_fetch.py
@@ -1,6 +1,7 @@
 import tweepy as tw
+import requests
 
-# your Twitter API key and API secret
+# Twitter API key and API secret
 my_api_key = "vhtMACAJiuqAOBHlGtGQkOQzw"
 my_api_secret = "Q2f3umR9davc18AmdGYPd0QQbgduDJgN3DUGLwI7QmH0rdoMlw"
 
@@ -8,18 +9,45 @@ my_api_secret = "Q2f3umR9davc18AmdGYPd0QQbgduDJgN3DUGLwI7QmH0rdoMlw"
 auth = tw.OAuth2AppHandler(my_api_key, my_api_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
+
 def get_tweets(keyword):
-    """Gets a list of 5 tweets based on a keyword
+    '''Gets a list of 4 tweets based on a keyword
 
     Args:
         keyword (str): The keyword to search for
 
     Returns:
         a list of tweet objects
-    """
-    query = "{} -filter:retweets lang:en".format(keyword)
-    tweets = api.search_tweets(query, tweet_mode = 'extended', count = 5)
+    '''
+    query = '"{}" -filter:retweets -filter:replies lang:en'.format(keyword)
+    tweets = api.search_tweets(query, tweet_mode = 'extended', count = 4)
     return tweets
 
+def get_ids(tweets):
+    '''Get two lists of tweet embedded tweets from a list of tweets
+
+    Args:
+        tweets (list): A list of tweet objects
+
+    Returns:
+        left_tweets and right_tweets (list): lists of embedded tweets
+    '''
+    left_tweets = []
+    right_tweets = []
+    i = 0
+    for tweet in tweets:
+        twt_http = requests.get('https://publish.twitter.com/oembed?url=https://twitter.com/twitter/status/' + tweet.id_str + '&dnt=false&maxwidth=180&maxheight=200')
+        twtjson = twt_http.json()
+        emb_tweet = twtjson.get('html')
+        # sort into two lists
+        if i // 2 == 0:
+            left_tweets.append(emb_tweet)
+        else:
+            right_tweets.append(emb_tweet)
+        i += 1
+    return left_tweets, right_tweets
 
 
+t = get_tweets('#yes')
+h = get_ids(t)
+print(h)
