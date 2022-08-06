e0bcb701448dc2028d9a01457eed0ed2f0159a92
ethanchung936 <ethanchung936@gmail.com>
2022-08-06 14:51:50 -0600

    Changed variable names to make more readable and added two sided tweet functionality

--- TwitWord/word_lookup/views.py
+++ TwitWord/word_lookup/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .tweet_fetch import get_tweets
+from .tweet_fetch import get_tweets, get_ids
 
 def home(request):
     # display the homepage and redirect to the word input page when sent a post request
     if request.method == 'POST':
         url = reverse('home')
-        word = request.POST['word']
+        user_input = request.POST['user_input']
         # TODO add word validation
-        return HttpResponseRedirect(url + word)
+        return HttpResponseRedirect(url + user_input)
     return render(request, 'word_lookup/home.html')
 
 def word_input(request, word):
     if request.method == 'POST':
         url = reverse('home')
-        word = request.POST['word']
+        user_input = request.POST['user_input']
         # TODO add word validation
-        return HttpResponseRedirect(url + word)
+        return HttpResponseRedirect(url + user_input)
     # Retrive the input word and process it
-    tweets = get_tweets(word)
-    return render(request, 'word_lookup/tweets.html', {'word': word})
+    raw_tweets = get_tweets(word)
+    left_tweets, right_tweets = get_ids(raw_tweets)
+    return render(request, 'word_lookup/tweets.html', {'left_tweets':left_tweets, 'right_tweets':right_tweets})
