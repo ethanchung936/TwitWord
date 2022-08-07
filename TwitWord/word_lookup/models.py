204b415962a351860a26674bf86cca40ebcfa73d
ethanchung936 <ethanchung936@gmail.com>
2022-08-06 19:51:49 -0600

    created a model to contain the user word searches

--- TwitWord/word_lookup/models.py
+++ TwitWord/word_lookup/models.py
@@ -1,3 +1,10 @@
+from tkinter.filedialog import test
 from django.db import models
+import uuid
 
-# Create your models here.
+class WordTable(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    word = models.CharField(max_length=100)
+    def __str__(self):
+        return self.word
+
