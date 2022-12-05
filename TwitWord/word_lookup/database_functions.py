d6e1cc009ec78f054204f4fab44311edf7a1ee5f
Ethan Chung <ethanchung936@gmail.com>
2022-12-04 18:41:18 -0700

    added more database functions

--- TwitWord/word_lookup/database_functions.py
+++ TwitWord/word_lookup/database_functions.py
@@ -9,16 +9,37 @@ from word_lookup.models import WordTable
 def delete_WordTable_contents():
     WordTable.objects.all().delete()
     
-num = input("1: Delete all contents of WordTable\n2: Exit\n")
+def view_WordTable_stats():
+    entry_dict = {}
+    entries = WordTable.objects.all()
+    for entry in entries:
+        if entry.word in entry_dict:
+            entry_dict[entry.word] += 1
+        else:
+            entry_dict[entry.word] = 1
+    print('WordTable contains {} entries.'.format(WordTable.objects.count()))
+    print(entry_dict)
+    
+promt_text = "1: Delete all contents of WordTable\n2: View database stats\n3: Exit\n"
+    
+option = input(promt_text)
 run = True
 while run:
-    if num == '1':
-        delete_WordTable_contents()
-        print("WordTable contents deleted\n")
-        num = input("1: Delete all contents of WordTable\n2: Exit\n")
-    elif num == '2':
+    if option == '1':
+        confirm = input("Are you sure you want to delete all contents of WordTable? (y/n)\n")
+        if confirm == 'y':
+            delete_WordTable_contents()
+            print("WordTable contents deleted\n")
+        else:
+            print("Cancelled\n")
+        option = input(promt_text)
+    elif option == '2':
+        view_WordTable_stats()
+        print()
+        option = input(promt_text)
+    elif option == '3':
         run = False
         print("Exiting")
     else:
         print("Invalid input\n")
-        num = input("1: Delete all contents of WordTable\n2: Exit\n")
+        option = input(promt_text)
